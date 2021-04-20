5f645a57a07f2101b023af598075fa8a8477f4ff
triwahyuu <triwahyu34@gmail.com>
2021-04-20 15:12:51 +0700

    update deploy

--- docs/conf.py
+++ docs/conf.py
@@ -113,16 +113,16 @@ autosectionlabel_prefix_document = True
 # -- Options for 'sphinx-multiversion' extension ------------------------------
 
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'^v.*$'
+smv_tag_whitelist = r'^.*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
 smv_branch_whitelist = r'^master$'
 
-# Pattern for released versions
-smv_released_pattern = r'^tags/v.*$'
+# # Pattern for released versions
+# smv_released_pattern = r'^tags/v.*$'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = None
+smv_remote_whitelist = r"^origin$"
 
 # Format for versioned output directories inside the build directory
 smv_outputdir_format = '{ref.name}'
